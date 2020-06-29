01afc89c97ece829206838b6fc0c0e2322c366cd
emelyanovkonstantin <emelyanovkonstantin@yandex.ru>
2020-06-30 02:18:11 +0300

    add Role

---
+++ src/java/ru/emelyanovkonstantin/springsecurity/model/User.java
@@ -0,0 +1,15 @@
+package ru.emelyanovkonstantin.springsecurity.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by EmelyanovKonstantin on 30.06.2020
+ *
+ * Simple JavaBean object that represent a  User
+ * @version 1.0
+ */
+@Entity
+@Table(name = "user")
+public class User {
+}
