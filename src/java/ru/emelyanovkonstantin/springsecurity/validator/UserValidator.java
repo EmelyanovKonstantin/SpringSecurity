7b31ec790ea66e1e3f994afb161e0210ffb0bc7c
emelyanovkonstantin <emelyanovkonstantin@yandex.ru>
2020-07-01 01:56:19 +0300

    add Validator

---
+++ src/java/ru/emelyanovkonstantin/springsecurity/validator/UserValidator.java
@@ -0,0 +1,49 @@
+package ru.emelyanovkonstantin.springsecurity.validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+import ru.emelyanovkonstantin.springsecurity.model.User;
+import ru.emelyanovkonstantin.springsecurity.service.UserService;
+
+/**
+ * Created by EmelyanovKonstantin on 01.07.2020
+ *
+ * implements {@link org.springframework.validation.Validator}
+ * @version 1.0
+ */
+public class UserValidator implements Validator {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return User.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        User user = (User) o;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"username","Require");
+        if(user.getUsername().length() < 6 ||  user.getUsername().length() >32){
+            errors.rejectValue("username", "Size.userForm.username");
+        }
+
+        if(userService.findByUsername(user.getUsername()) != null){
+            errors.rejectValue("username", "Duplicate.userForm.username");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"password", "Required");
+        if(user.getPassword().length() < 6 || user.getPassword().length() > 32){
+            errors.rejectValue("password","Size.userForm.password");
+        }
+
+        if(!user.getConfirmPassword().equals(user.getPassword())){
+            errors.rejectValue("ConfirmPassword","Different.userForm.password");
+        }
+
+    }
+}
